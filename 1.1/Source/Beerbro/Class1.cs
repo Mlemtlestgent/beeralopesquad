f8b98db243f1a3ff98641b9f397baaf969e57259
Mlemtlestgent <78567297+Mlemtlestgent@users.noreply.github.com>
2025-06-17 07:33:51 -0500

    Add files via upload

---
+++ 1.1/Source/Beerbro/Class1.cs
@@ -0,0 +1,27 @@
+﻿using Verse;
+using RimWorld;
+
+namespace BeeralopeActions
+{
+    public class DeathActionWorker_ExtremeExplosion : DeathActionWorker
+
+    {
+        public override void PawnDied(Corpse corpse)
+        {
+            float radius;
+            if (corpse.InnerPawn.ageTracker.CurLifeStageIndex == 0)
+            {
+                radius = 1.9f;
+            }
+            else if (corpse.InnerPawn.ageTracker.CurLifeStageIndex == 1)
+            {
+                radius = 3.9f;
+            }
+            else
+            {
+                radius = 8.9f;
+            }
+            GenExplosion.DoExplosion(corpse.Position, corpse.Map, radius, DamageDefOf.Flame, corpse.InnerPawn, -1, -1f, null, null, null, null, null, 0f, 1, false, null, 0f, 1, 0f, false, null, null);
+        }
+    }
+}
---
+++ 1.1/Source/Beerlope/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace Beerlope
+{
+    [StaticConstructorOnStartup]
+    public static class Class1
+    {
+        static Class1() //our constructor
+        {
+            Log.Message("Hello World!"); //Outputs "Hello World!" to the dev console.
+        }
+    }
+}
